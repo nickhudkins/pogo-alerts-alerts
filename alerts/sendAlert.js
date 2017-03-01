19b933b6dd74c3b2bc3362be2011b40c25c1c78c
hokiepokedad <pogovaalerts@gmail.com>
2017-02-28 19:24:34 -0500

    Fixed moment reference

--- alerts/sendAlert.js
+++ alerts/sendAlert.js
@@ -1,12 +1,13 @@
 const logger = require('../logging');
 const request = require('request');
+const moment = require('moment');
 
 const doPost = (payload) => {
   return new Promise((resolve, reject) => {
-    request.post('https://discordapp.com/api/webhooks/286185098548740096/kQph4c9gkvC99c0ZGUawEOrnQ3h3RHvwX73eOOi_gOhxaogHUSavzQwWHSpTS0QDGa7V', {
+    request.post(process.env.WEBHOOK_URL || 'http://requestb.in/xxch05xx', {
 		json: {
 			username: 'Twitter Listener',
-			content: `**${payload.accountName}** has been down for over an hour. The last tweet was ${moment(payload.lastTweet.createdAt).fromNow()}. Check on it ASAP!`
+			content: `**${payload.accountName}** appears to be down. The last tweet was ${moment(payload.lastTweet.createdAt).fromNow()}. Check on it ASAP!`
 		}			
     }, (err, resp) => {
       if (err) reject(err);

3f666587fff9563ed7491fd285e6138aa4db7546
hokiepokedad <pogovaalerts@gmail.com>
2017-03-01 18:59:05 -0500

    Modified alert template to check for last tweet existence.

--- alerts/sendAlert.js
+++ alerts/sendAlert.js
@@ -4,10 +4,15 @@ const moment = require('moment');
 
 const doPost = (payload) => {
   return new Promise((resolve, reject) => {
+	const lastTweetText = 'never found';
+	if (payload.lastTweet) {
+		lastTweetText = moment(payload.lastTweet.createdAt).fromNow();
+	}
+	  
     request.post(process.env.WEBHOOK_URL || 'http://requestb.in/xxch05xx', {
 		json: {
 			username: 'Twitter Listener',
-			content: `@everyone **${payload.accountName}** appears to be down. The last tweet was ${moment(payload.lastTweet.createdAt).fromNow()}. Check on it ASAP!`
+			content: `@everyone **${payload.accountName}** appears to be down. The last tweet was ${lastTweetText}. Check on it ASAP!`
 		}			
     }, (err, resp) => {
       if (err) reject(err);
